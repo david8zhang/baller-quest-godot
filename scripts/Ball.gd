3d1ded7b2abe32114bfb004cab20f393fee45ea7
David Zhang <david8zhang@berkeley.edu>
2024-06-12 18:02:01 -0700

    added bounds for court

--- scripts/Ball.gd
+++ scripts/Ball.gd
@@ -28,6 +28,7 @@ func _on_player_detector_body_entered(body):
 	var player = body as Player
 	player.receive_pass(self)
 
+
 # logic for when ball colliders with hoop on makes
 func _on_make_detector_body_entered(body: Node2D):
 	if body.name == "NetCollider":
@@ -35,3 +36,12 @@ func _on_make_detector_body_entered(body: Node2D):
 		linear_velocity.x = linear_velocity.x * 0.25
 		linear_velocity.y = linear_velocity.y * 0.75
 		enable_collision()
+
+
+func _physics_process(delta):
+	screen_size = get_viewport_rect().size
+	var right_bound = screen_size.x / 2
+	var left_bound = -screen_size.x / 2
+	var lower_bound = screen_size.y / 2
+	if global_position.x > right_bound || global_position.x < left_bound || global_position.y > lower_bound:
+		queue_free()
