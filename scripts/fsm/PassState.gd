64c4251129a322614441fa7ad51473a0cda5d7e2
david8zhang <david8zhang@berkeley.edu>
2024-07-14 13:35:51 -0700

    added pass animations

--- scripts/fsm/PassState.gd
+++ scripts/fsm/PassState.gd
@@ -2,6 +2,34 @@ class_name PassState
 extends State
 
 func enter(msg := {}) -> void:
-	anim_sprite.play("idle-front")
 	var player = entity as Player
-	player.pass_ball()
+	var pass_target = player.pass_target
+	var pass_target_pos = pass_target.global_position
+	var player_pos = player.global_position
+	var x_diff = abs(pass_target_pos.x - player_pos.x)
+	var y_diff = abs(pass_target_pos.y - player_pos.y)
+	
+	# Determine whether to play side-pass animation or vertical-pass animation
+	var pass_axis = "vertical"
+	if x_diff > 50:
+		pass_axis = "horizontal"
+	
+	if pass_axis == "vertical":
+		if pass_target_pos.y > player_pos.y:
+			anim_sprite.play("pass-front")
+		else:
+			anim_sprite.play("pass-back")
+	elif pass_axis == "horizontal":
+		if pass_target_pos.x > player_pos.x:
+			anim_sprite.flip_h = false
+		else:
+			anim_sprite.flip_h = true
+		anim_sprite.play("pass-side")
+	anim_sprite.frame_changed.connect(_on_pass_anim_frame)
+
+
+func _on_pass_anim_frame():
+	if anim_sprite.frame == 3:
+		var player = entity as Player
+		player.pass_ball()
+		anim_sprite.frame_changed.disconnect(_on_pass_anim_frame)
