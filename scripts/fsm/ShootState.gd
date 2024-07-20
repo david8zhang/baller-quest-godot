195fa41533685db4499f27032abc9da3ee2ad1b1
david8zhang <david8zhang@berkeley.edu>
2024-07-20 11:43:41 -0700

    added shoot side animation

--- scripts/fsm/ShootState.gd
+++ scripts/fsm/ShootState.gd
@@ -22,7 +22,14 @@ func handle_input(input: InputEvent) -> void:
 		is_released = true
 
 func enter(msg:={}):
-	anim_sprite.play("shoot-windup")
+	var anim_name = "shoot-%s-windup" % _get_shoot_anim_angle()
+	if anim_name == "shoot-side-windup":
+		var player = entity as Player
+		var x_diff = player.global_position.x - player.hoop.global_position.x
+		anim_sprite.flip_h = x_diff > 0
+	else:
+		anim_sprite.flip_h = false
+	anim_sprite.play(anim_name)
 	anim_sprite.animation_finished.connect(_shoot_windup_complete)
 	
 func exit():
@@ -30,8 +37,9 @@ func exit():
 	
 func _shoot_windup_complete():
 	var player = entity as Player
+	var anim_name = "shoot-%s-jump" % _get_shoot_anim_angle()
 	anim_sprite.animation_finished.disconnect(_shoot_windup_complete)
-	anim_sprite.play("shoot-jump")
+	anim_sprite.play(anim_name)
 	
 	var on_jump_complete_callable = Callable(self, "on_jump_complete")
 	var on_jump_peak_callable = Callable(self, "on_jump_peak")
@@ -40,7 +48,8 @@ func _shoot_windup_complete():
 
 func on_jump_complete():
 	var player = entity as Player
-	anim_sprite.play("shoot-land")
+	var anim_name = "shoot-%s-land" % _get_shoot_anim_angle()
+	anim_sprite.play(anim_name)
 	var timer = Timer.new()
 	timer.autostart = true
 	timer.one_shot = true
@@ -51,10 +60,16 @@ func on_jump_complete():
 
 func on_jump_peak():
 	var player = entity as Player
-	anim_sprite.play("shoot-release")
+	var anim_name = "shoot-%s-release" % _get_shoot_anim_angle()
+	anim_sprite.play(anim_name)
 	player.shoot_ball(shot_result)
 
 
 func _follow_through_complete():
 	var player = entity as Player
 	player._state_machine.transition_to("IdleState", {})
+	
+func _get_shoot_anim_angle():
+	var player = entity as Player
+	var x_diff = abs(player.global_position.x - player.hoop.global_position.x)
+	return "side" if x_diff > 250 else "front"
