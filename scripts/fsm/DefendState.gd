1b3b971242957f62bef3ab5789abc1b749f2c7fb
david8zhang <david8zhang@berkeley.edu>
2024-08-27 00:00:28 -0400

    added defensive movement

--- scripts/fsm/DefendState.gd
+++ scripts/fsm/DefendState.gd
@@ -1,6 +1,10 @@
 class_name DefendState
 extends State
 
+const SPEED = 300.0
+const MAX_DISTANCE = 15
+var last_moved_time = 0
+
 func enter(msg:={}):
 	var anim_name = "onball-defend-front"
 	var player = entity as Player
@@ -15,3 +19,32 @@ func update(_delta: float):
 	var player_to_defend = player.get_player_to_defend() as Player
 	if !player_to_defend.has_ball:
 		player._state_machine.transition_to("IdleState")
+
+
+func _physics_process(delta):
+	var curr_player = entity as Player
+	var player_to_defend = curr_player.get_player_to_defend() as Player
+	
+	if player_to_defend != null and player_to_defend.has_ball:
+		var player_to_defend_pos = player_to_defend.global_position
+		var game = curr_player.game as Game
+		var hoop = game.hoop_1 if curr_player.side == Game.SIDE.CPU else game.hoop_2 as Hoop
+
+		# Get to quarter point between player to defend and hoop
+		var mid_to_hoop = Vector2((player_to_defend_pos.x + hoop.global_position.x) / 2, (player_to_defend_pos.y + hoop.global_position.y) / 2)
+		var quarter_to_hoop = Vector2((player_to_defend_pos.x + mid_to_hoop.x) / 2, (player_to_defend_pos.y + mid_to_hoop.y) / 2)
+
+		# move toward defense point
+		var dir = (quarter_to_hoop - curr_player.global_position).normalized()
+		if (curr_player.global_position - quarter_to_hoop).length() > MAX_DISTANCE:
+			curr_player.translate(dir * delta * SPEED)
+			anim_sprite.play("onball-defensive-slide")
+			anim_sprite.flip_h = dir.x > 0
+			last_moved_time = Time.get_unix_time_from_system() * 1000
+		else:
+			var curr_time = Time.get_unix_time_from_system() * 1000
+			if curr_time - last_moved_time > 250:
+				var anim_name = "onball-defend-front"
+				if curr_player.side == Game.SIDE.CPU:
+					anim_name = "cpu-" + anim_name
+				anim_sprite.play(anim_name)
--- scripts/fsm/MoveState.gd
+++ scripts/fsm/MoveState.gd
@@ -1,5 +1,20 @@
 class_name MoveState
 extends State
 
+const SPEED = 300.0
+var dest_point: Vector2
+
 func enter(msg:={}):
+	dest_point = msg["dest_point"]
+
+func _physics_process(delta):
+	print("Move state!")
 	pass
+#	var player = entity as Player
+#	if dest_point != null:
+#		var angle_between = player.global_position.angle_to(dest_point)
+#		var x_dir = cos(angle_between)
+#		var y_dir = sin(angle_between)
+#		var velocity = Vector2(x_dir, y_dir) * SPEED
+#		player.global_position += velocity * delta
+		
