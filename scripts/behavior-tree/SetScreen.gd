f5e9f90d5cd29840c3185d6a89c2821fa5850eb9
david8zhang <david8zhang@berkeley.edu>
2024-09-29 12:45:13 -0700

    fix screen sliding bug

--- scripts/behavior-tree/SetScreen.gd
+++ scripts/behavior-tree/SetScreen.gd
@@ -4,12 +4,14 @@ extends ActionLeaf
 func tick(actor: Node, blackboard: Blackboard):
   var curr_player = actor as CourtPlayer
   curr_player.is_screen_active = true
+  var game = blackboard.get_value("Game") as Game
+  
+  var ball_handler = game.get_ball_handler()
+  if ball_handler != null && ball_handler.get_defender() != null:
+    ball_handler.get_defender().set_collision_mask_value(1, true)
 
   # Make curr player immovable
   curr_player.linear_velocity = Vector2.ZERO
-  print(curr_player.linear_velocity)
-  curr_player.linear_damp = 100
   curr_player.anim_sprite.flip_h = true
   curr_player.anim_sprite.play("set-screen-side")
-  curr_player.set_collision_mask_value(1, true)
   return SUCCESS
